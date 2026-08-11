5b48dfd605c38f55f655ec5cf7b9d2a51587c3a8
John W. Long <me@johnwlong.com>
2026-08-11 17:18:19 -0400

    Seed prbadge cask (v0.1.0 placeholder; release.sh updates version+sha256)

---
+++ Casks/prbadge.rb
@@ -0,0 +1,13 @@
+cask "prbadge" do
+  version "0.1.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/32pixelsCo/PRBadge-releases/releases/download/v#{version}/PRBadge-#{version}.zip"
+  name "PRBadge"
+  desc "Menu bar app for open PRs you authored"
+  homepage "https://github.com/32pixelsCo/PRBadge"
+
+  app "PRBadge.app"
+
+  zap trash: "~/Library/Preferences/co.32pixels.prbadge.plist"
+end
